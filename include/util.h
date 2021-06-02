f04e0cbfa6be735ccd2b4c2a1825edbdb6eb95e2
pcbend <pcbend@gmail.com>
2021-06-02 13:58:16 -0400

    made cmake work with root

---
+++ include/util.h
@@ -0,0 +1,8 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include <cstdio>
+
+void Hello() { printf("hello!\n"); }
+
+#endif
--- src/bcssort.cxx
+++ src/bcssort.cxx
@@ -1,11 +1,16 @@
 
 
 
+#include <util.h>
 
-
+#include <TH1D.h>
 
 int main(int argc, char **argv){
 
+    Hello();
+    printf("yo\n");
+
+    TH1D h("h","h",100,0,100);
 
     return 0;
 }
