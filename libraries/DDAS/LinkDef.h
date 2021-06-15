4243153cfefc764b842b49be9bcd4beea4cdc596
RHYME11 <amaydaygo@gmail.com>
2021-06-15 12:12:38 -0400

    started adding useful root libraries/dicts

---
+++ libraries/DDAS/LinkDef.h
@@ -0,0 +1,15 @@
+// DDASEvent.h DDASHit.h ddaschannel.h
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class DDASEvent+;
+#pragma link C++ class DDASHit+;
+#pragma link C++ class ddaschannel+;
+#pragma link C++ class std::vector<ddaschannel>+;
+
+#endif
